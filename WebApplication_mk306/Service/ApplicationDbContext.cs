f8f24efd96d6ce8b6b1e137d227c58b0045871ef
krishnanm <krishnanm@poconnor.com>
2026-02-14 16:36:51 +0530

    connection string added

---
+++ WebApplication_mk306/Service/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication_mk306.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+}
